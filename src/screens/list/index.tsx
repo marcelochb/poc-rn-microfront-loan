471bfcbef03f5e2df5266691a42ddd3f92bbbec6
Marcelo Block Teixeira <marcelochb@gmail.com>
2023-02-21 01:01:53 -0300

    using loan Controller

--- src/screens/list/index.tsx
+++ src/screens/list/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { ThemeBase } from '@poc/theme'
 import { LoanListTemplate } from '@poc/templates'
+import { useLoanListController } from '@poc/core'
 
 export const LoanList = () => {
+  const {getController, handleController} = useLoanListController();
   return (
-    <LoanListTemplate theme={ThemeBase.Midway} />
+    <LoanListTemplate theme={ThemeBase.Midway} loans={getController.loans} />
   )
 }
