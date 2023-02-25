11accfe3a10d683dfe72ccedbd05b6f15af2ceb9
Marcelo Block Teixeira <marcelochb@gmail.com>
2023-02-24 23:08:58 -0300

    Loan detail

--- src/screens/detail/index.tsx
+++ src/screens/detail/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { ThemeBase } from '@poc/theme';
 import { LoanDetailTemplate } from '@poc/templates';
+import { useLoanDetailController } from '@poc/core';
 
 export const LoanDetailScreen = () => {
+  const {getController} = useLoanDetailController();
   return (
-    <LoanDetailTemplate theme={ThemeBase.Midway} />
+    <LoanDetailTemplate 
+      theme={ThemeBase.Midway}
+      loading={getController.loading}
+      error={getController.error}
+      loan={getController.loan}
+    />
   );
 };
