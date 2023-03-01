d878903a97f07c7d6914d9bf2f8b035150edab91
Marcelo Block Teixeira <marcelochb@gmail.com>
2023-02-28 21:01:14 -0300

    feat: LoanListScreen

--- src/screens/list/index.tsx
+++ src/screens/list/index.tsx
@@ -1,14 +1,14 @@
 import React, { useCallback } from 'react'
 import { ThemeBase } from '@poc/theme'
 import { LoanListTemplate } from '@poc/templates'
-import { useLoanListController } from '@poc/core'
+import { LoanEntity, useLoanListController } from '@poc/core'
 import { useNavigation } from '@react-navigation/native'
 import { LOAN_NAVIGATORS } from '@poc/tools'
 
 export const LoanListScreen = () => {
   const navigation = useNavigation<any>();
   const navigateToDetail = useCallback(
-      (id: string) => {
+      ({id}:LoanEntity) => {
       navigation.navigate(LOAN_NAVIGATORS.screens.detail.name, {id});
     },[]
   )
@@ -16,10 +16,10 @@ export const LoanListScreen = () => {
   return (
     <LoanListTemplate 
       theme={ThemeBase.Midway} 
-      loans={getController.data} 
+      listData={getController.data} 
       error={getController.error}
       loading={getController.loading}
-      navigateToDetail={navigateToDetail}
+      callBack={navigateToDetail}
     />
   )
 }
