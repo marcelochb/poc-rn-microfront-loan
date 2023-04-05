5c4596de66fb098ec8567366156e373986a6b3b3
Marcelo Block Teixeira <marcelochb@gmail.com>
2023-04-04 23:10:17 -0300

    feat: pegando tema do estado global

--- src/screens/create/index.tsx
+++ src/screens/create/index.tsx
@@ -3,14 +3,17 @@ import { LoanCreateTemplate } from '@poc/templates'
 import { ThemeBase } from '@poc/theme'
 import { useLoanCreateController } from '@poc/core'
 import { useNavigation } from '@react-navigation/native'
+import { useSelector } from 'react-redux'
+import { IGlobalState } from '@poc/interfaces'
 
 export const LoanCreateScreen = () => {
+  const theme = useSelector((state:IGlobalState) => state.theme.value);
   const navigation = useNavigation();
   const {data,error,loading,onChange,onSubmit} = useLoanCreateController(navigation.goBack);
   return (
     <LoanCreateTemplate
       error={error}
-      theme={ThemeBase.Midway}
+      theme={theme}
       nameLabel='Nome'
       nameValue={data.name}
       nameChangeText={onChange('name')}
--- src/screens/detail/index.tsx
+++ src/screens/detail/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { ThemeBase } from '@poc/theme';
 import { LoanDetailTemplate } from '@poc/templates';
 import { useLoanDetailController } from '@poc/core';
+import { useSelector } from 'react-redux';
+import { IGlobalState } from '@poc/interfaces';
 
 export const LoanDetailScreen = () => {
+  const theme = useSelector((state:IGlobalState) => state.theme.value);
   const {loading,error,data} = useLoanDetailController();
   return (
     <LoanDetailTemplate 
-      theme={ThemeBase.Midway}
+      theme={theme}
       loading={loading}
       error={error}
       data={data}
